a3ddb057dd79b273b4e4cc044c1c016997a8cd72
Abdul Shaikh <abdul@Abdulamite.local>
2019-05-13 00:49:37 -0700

    complete challenge3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//The pupose of this file to gain practice working on common and advanced coding questions to
+// The pupose of this file to gain practice working on common and advanced coding questions to
 // improve my own quality of code and overall programming knowledge
 
 //1. Write a function to reverse a given string.
@@ -36,3 +36,31 @@ console.log(simpleAdding(3) === 6);
 console.log(simpleAdding(4) === 10);
 console.log(simpleAdding(12) === 78);
 console.log(simpleAdding(140) === 9870);
+
+
+//3. Write a function that will return true if the letters in the string are between two + symbols
+function simpleSymbol(str){
+    let split = str.split('');
+    let reg = RegExp(/^[a-zA-Z]+$/);
+    let errors = 0;
+    for(let i = 0; i<=split.length-1;i++){
+        if(reg.test(split[i])){
+            let back = (split[i-1]);
+            let forward = (split[i+1]);
+            if(back === '+' && forward === '+'){
+            }else{
+                errors+=1;
+            }
+        }
+    }
+    if(errors>0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+
+console.log(simpleSymbol("+d+=3=+s+") === true);
+console.log(simpleSymbol("f++d+") === false);
